31e699d5d01d0cc4846cb35acb2c98040fa32c30
Alex Jercan <a.jercan@accenture.com>
2022-07-11 10:57:29 +0300

    added day 01

--- src/day01.py
+++ src/day01.py
@@ -1,8 +1,18 @@
-def part1(input):
-  return input
+def part1(input: str) -> int:
+  return sum(map(int, input.splitlines()))
 
-def part2(input):
-  return input
+def part2(input: str) -> int:
+  adjustments = list(map(int, input.splitlines()))
+  visited = set()
+  freq = 0
+
+  while True:
+    for a in adjustments:
+        freq = freq + a
+        if freq in visited:
+          return freq
+        visited.add(freq)
+
 
 def solve(input: str) -> str:
   return f"Day01\nPart1: {part1(input)}\nPart2: {part2(input)}\n"
