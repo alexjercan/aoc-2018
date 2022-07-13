364dbf53d0d7d4becbb512eb5d1e131d0defc2de
Alex Jercan <a.jercan@accenture.com>
2022-07-13 16:44:13 +0300

    added day 05

--- src/day05.py
+++ src/day05.py
@@ -1,8 +1,23 @@
-def part1(input):
-  return input
+def reacts(a, b):
+    return (a != b and a.upper() == b.upper())
 
-def part2(input):
-  return input
+def do_reaction(polymer):
+    buf = []
+    for c in polymer:
+        if buf and reacts(c, buf[-1]):
+            buf.pop()
+        else:
+            buf.append(c)
+    return len(buf)
+
+def part1(polymer: str):
+  return do_reaction(polymer)
+
+def part2(polymer):
+  agents = set([c.lower() for c in polymer])
+  return min([do_reaction(polymer.replace(a, '').replace(a.upper(), '')) for a in agents])
 
 def solve(input: str) -> str:
+  input = input.strip()
+
   return f"Day05\nPart1: {part1(input)}\nPart2: {part2(input)}\n"
